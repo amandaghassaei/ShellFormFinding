9d35de3fbcc169805ddf38e76466bcae30c3a062
amandaghassaei <amandaghassaei@gmail.com>
2016-10-07 18:44:28 -0400

    simplify dynamic logic

--- js/dynamicModel.js
+++ js/dynamicModel.js
@@ -32,6 +32,10 @@ function initDynamicModel(globals){
     var velocity;
     var lastVelocity;
     var externalForces;
+    var mass;
+    var meta;
+    var beamK;
+    var beamD;
 
     runSolver();
 
@@ -56,20 +60,20 @@ function initDynamicModel(globals){
     function solveStep(){
 
         for (var i=0;i<nodes.length;i++){
-
-            var node = nodes[i];
             var rgbaIndex = i*4;
 
-            if (node.fixed) continue;
-            var force = node.getExternalForce();
+            if (mass[rgbaIndex+1] == 1) continue;
+            var force = new THREE.Vector3(externalForces[rgbaIndex], externalForces[rgbaIndex+1], externalForces[rgbaIndex+2]);
             var nodePosition = new THREE.Vector3(lastPosition[rgbaIndex], lastPosition[rgbaIndex+1], lastPosition[rgbaIndex+2]);
             var nodeVelocity = new THREE.Vector3(lastVelocity[rgbaIndex], lastVelocity[rgbaIndex+1], lastVelocity[rgbaIndex+2]);
             var nodeOrigPosition = new THREE.Vector3(originalPosition[rgbaIndex], originalPosition[rgbaIndex+1], originalPosition[rgbaIndex+2]);
 
-            for (var j=0;j<node.beams.length;j++){
-                var beam = node.beams[j];
-                var neighborIndex = (beam.getOtherNode(node)).getIndex()*4;
-
+            for (var j=0;j<4;j++){
+                var neighborIndex = meta[rgbaIndex+j]*4;
+                if (neighborIndex<0){
+                    //no beam
+                    continue;
+                }
                 var neighborPosition = new THREE.Vector3(lastPosition[neighborIndex], lastPosition[neighborIndex+1], lastPosition[neighborIndex+2]);
                 var neighborVelocity = new THREE.Vector3(lastVelocity[neighborIndex], lastVelocity[neighborIndex+1], lastVelocity[neighborIndex+2]);
                 var neighborOrigPosition = new THREE.Vector3(originalPosition[neighborIndex], originalPosition[neighborIndex+1], originalPosition[neighborIndex+2]);
@@ -78,14 +82,14 @@ function initDynamicModel(globals){
                 var deltaP = neighborPosition.sub(nodePosition).add(nominalDist);
                 deltaP.sub(deltaP.clone().normalize().multiplyScalar(nominalDist.length()));
                 var deltaV = neighborVelocity.sub(nodeVelocity);
-                var _force = deltaP.multiplyScalar(beam.getK()).add(deltaV.multiplyScalar(beam.getD()));
+                var _force = deltaP.multiplyScalar(beamK[rgbaIndex+j]).add(deltaV.multiplyScalar(beamD[rgbaIndex+j]));
                 force.add(_force);
             }
 
             //euler integration
-            var mass = node.getMass();
+            var _mass = mass[rgbaIndex];
 
-            nodeVelocity = force.multiplyScalar(dt/mass).add(nodeVelocity);
+            nodeVelocity = force.multiplyScalar(dt/_mass).add(nodeVelocity);
             velocity[rgbaIndex] = nodeVelocity.x;
             velocity[rgbaIndex+1] = nodeVelocity.y;
             velocity[rgbaIndex+2] = nodeVelocity.z;
@@ -95,7 +99,6 @@ function initDynamicModel(globals){
             position[rgbaIndex+1] = nodePosition.y;
             position[rgbaIndex+2] = nodePosition.z;
         }
-        //console.log(originalPosition);
 
         var temp = lastPosition;
         lastPosition = position;
@@ -139,6 +142,10 @@ function initDynamicModel(globals){
         velocity = new Float32Array(numNodes*4);
         lastVelocity = new Float32Array(numNodes*4);
         externalForces = new Float32Array(numNodes*4);
+        mass = new Float32Array(numNodes*4);
+        meta = new Int8Array(numNodes*4);
+        beamK = new Float32Array(numNodes*4);
+        beamD = new Float32Array(numNodes*4);
 
         _.each(nodes, function(node, index){
             var externalForce = node.getExternalForce();
@@ -149,6 +156,19 @@ function initDynamicModel(globals){
             originalPosition[4*index] = origPosition.x;
             originalPosition[4*index+1] = origPosition.y;
             originalPosition[4*index+2] = origPosition.z;
+            mass[4*index] = node.getMass();
+            mass[4*index+1] = (node.fixed ? 1 : 0);
+
+            meta[4*index] = -1;
+            meta[4*index+1] = -1;
+            meta[4*index+2] = -1;
+            meta[4*index+3] = -1;
+            _.each(node.beams, function(beam, i){
+                meta[4*index+i] = beam.getOtherNode(node).getIndex();
+                beamK[4*index+i] = beam.getK();
+                beamD[4*index+i] = beam.getD();
+            });
+
         });
     }
 
