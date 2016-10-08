a1f9a80cbc8918a9a53d3b5e0e455a1ee659615e
amandaghassaei <amandaghassaei@gmail.com>
2016-10-07 20:49:29 -0400

    math on the gpu

--- js/dynamicModel.js
+++ js/dynamicModel.js
@@ -5,7 +5,6 @@
 function initDynamicModel(globals){
 
     var object3D = new THREE.Object3D();
-    //object3D.position.y = globals.planeHeight;
     globals.threeView.sceneAdd(object3D);
 
     var schematic = globals.schematic;
@@ -37,6 +36,8 @@ function initDynamicModel(globals){
     var beamK;
     var beamD;
 
+    var textureDim = 0;
+
     runSolver();
 
     function runSolver(){
@@ -48,7 +49,6 @@ function initDynamicModel(globals){
 
         var gpuMath = globals.gpuMath;
         initTexturesAndPrograms(gpuMath);
-        gpuMath.setProgram("velocityCalc");
         gpuMath.setProgram("velocityCalc");
         gpuMath.setUniformForProgram("velocityCalc", "u_dt", dt, "1f");
         gpuMath.setProgram("positionCalc");
@@ -68,65 +68,93 @@ function initDynamicModel(globals){
 
     function solveStep(){
 
-        for (var i=0;i<nodes.length;i++){
-            var rgbaIndex = i*4;
+        var gpuMath = globals.gpuMath;
 
-            if (mass[rgbaIndex+1] == 1) continue;
-            var force = new THREE.Vector3(externalForces[rgbaIndex], externalForces[rgbaIndex+1], externalForces[rgbaIndex+2]);
-            var nodePosition = new THREE.Vector3(lastPosition[rgbaIndex], lastPosition[rgbaIndex+1], lastPosition[rgbaIndex+2]);
-            var nodeVelocity = new THREE.Vector3(lastVelocity[rgbaIndex], lastVelocity[rgbaIndex+1], lastVelocity[rgbaIndex+2]);
-            var nodeOrigPosition = new THREE.Vector3(originalPosition[rgbaIndex], originalPosition[rgbaIndex+1], originalPosition[rgbaIndex+2]);
+        gpuMath.step("velocityCalc", ["u_lastPosition", "u_lastVelocity", "u_originalPosition", "u_externalForces",
+            "u_mass", "u_meta", "u_beamK", "u_beamD"], "u_velocity");
+        gpuMath.step("positionCalc", ["u_velocity", "u_lastPosition", "u_mass"], "u_position");
 
-            for (var j=0;j<4;j++){
-                var neighborIndex = meta[rgbaIndex+j]*4;
-                if (neighborIndex<0){
-                    //no beam
-                    continue;
-                }
-                var neighborPosition = new THREE.Vector3(lastPosition[neighborIndex], lastPosition[neighborIndex+1], lastPosition[neighborIndex+2]);
-                var neighborVelocity = new THREE.Vector3(lastVelocity[neighborIndex], lastVelocity[neighborIndex+1], lastVelocity[neighborIndex+2]);
-                var neighborOrigPosition = new THREE.Vector3(originalPosition[neighborIndex], originalPosition[neighborIndex+1], originalPosition[neighborIndex+2]);
+        gpuMath.swapTextures("u_velocity", "u_lastVelocity");
+        gpuMath.swapTextures("u_position", "u_lastPosition");
 
-                var nominalDist = neighborOrigPosition.sub(nodeOrigPosition);
-                var deltaP = neighborPosition.sub(nodePosition).add(nominalDist);
-                deltaP.sub(deltaP.clone().normalize().multiplyScalar(nominalDist.length()));
-                var deltaV = neighborVelocity.sub(nodeVelocity);
-                var _force = deltaP.multiplyScalar(beamK[rgbaIndex+j]).add(deltaV.multiplyScalar(beamD[rgbaIndex+j]));
-                force.add(_force);
-            }
 
-            //euler integration
-            var _mass = mass[rgbaIndex];
-
-            nodeVelocity = force.multiplyScalar(dt/_mass).add(nodeVelocity);
-            velocity[rgbaIndex] = nodeVelocity.x;
-            velocity[rgbaIndex+1] = nodeVelocity.y;
-            velocity[rgbaIndex+2] = nodeVelocity.z;
-
-            nodePosition = nodeVelocity.multiplyScalar(dt).add(nodePosition);
-            position[rgbaIndex] = nodePosition.x;
-            position[rgbaIndex+1] = nodePosition.y;
-            position[rgbaIndex+2] = nodePosition.z;
-        }
-
-        var temp = lastPosition;
-        lastPosition = position;
-        position = temp;
-
-        temp = lastVelocity;
-        lastVelocity = velocity;
-        velocity = temp;
+        //for (var i=0;i<nodes.length;i++){
+        //    var rgbaIndex = i*4;
+        //
+        //    if (mass[rgbaIndex+1] == 1) continue;
+        //    var force = new THREE.Vector3(externalForces[rgbaIndex], externalForces[rgbaIndex+1], externalForces[rgbaIndex+2]);
+        //    var nodePosition = new THREE.Vector3(lastPosition[rgbaIndex], lastPosition[rgbaIndex+1], lastPosition[rgbaIndex+2]);
+        //    var nodeVelocity = new THREE.Vector3(lastVelocity[rgbaIndex], lastVelocity[rgbaIndex+1], lastVelocity[rgbaIndex+2]);
+        //    var nodeOrigPosition = new THREE.Vector3(originalPosition[rgbaIndex], originalPosition[rgbaIndex+1], originalPosition[rgbaIndex+2]);
+        //
+        //    for (var j=0;j<4;j++){
+        //        var neighborIndex = meta[rgbaIndex+j]*4;
+        //        if (neighborIndex<0){
+        //            //no beam
+        //            continue;
+        //        }
+        //        var neighborPosition = new THREE.Vector3(lastPosition[neighborIndex], lastPosition[neighborIndex+1], lastPosition[neighborIndex+2]);
+        //        var neighborVelocity = new THREE.Vector3(lastVelocity[neighborIndex], lastVelocity[neighborIndex+1], lastVelocity[neighborIndex+2]);
+        //        var neighborOrigPosition = new THREE.Vector3(originalPosition[neighborIndex], originalPosition[neighborIndex+1], originalPosition[neighborIndex+2]);
+        //
+        //        var nominalDist = neighborOrigPosition.sub(nodeOrigPosition);
+        //        var deltaP = neighborPosition.sub(nodePosition).add(nominalDist);
+        //        deltaP.sub(deltaP.clone().normalize().multiplyScalar(nominalDist.length()));
+        //        var deltaV = neighborVelocity.sub(nodeVelocity);
+        //        var _force = deltaP.multiplyScalar(beamK[rgbaIndex+j]).add(deltaV.multiplyScalar(beamD[rgbaIndex+j]));
+        //        force.add(_force);
+        //    }
+        //
+        //    //euler integration
+        //    var _mass = mass[rgbaIndex];
+        //
+        //    nodeVelocity = force.multiplyScalar(dt/_mass).add(nodeVelocity);
+        //    velocity[rgbaIndex] = nodeVelocity.x;
+        //    velocity[rgbaIndex+1] = nodeVelocity.y;
+        //    velocity[rgbaIndex+2] = nodeVelocity.z;
+        //
+        //    nodePosition = nodeVelocity.multiplyScalar(dt).add(nodePosition);
+        //    position[rgbaIndex] = nodePosition.x;
+        //    position[rgbaIndex+1] = nodePosition.y;
+        //    position[rgbaIndex+2] = nodePosition.z;
+        //}
+        //
+        //var temp = lastPosition;
+        //lastPosition = position;
+        //position = temp;
+        //
+        //temp = lastVelocity;
+        //lastVelocity = velocity;
+        //velocity = temp;
 
     }
 
     function render(){
-        for (var i=0;i<nodes.length;i++){
-            var node = nodes[i];
-            var rgbaIndex = i*4;
-            var nodePosition = new THREE.Vector3(lastPosition[rgbaIndex], lastPosition[rgbaIndex+1], lastPosition[rgbaIndex+2]);
-            node.render(nodePosition);
+
+        var vectorLength = 4;
+        globals.gpuMath.setProgram("packToBytes");
+        globals.gpuMath.setUniformForProgram("packToBytes", "u_vectorLength", vectorLength, "1f");
+        globals.gpuMath.setSize(textureDim*vectorLength, textureDim);
+        globals.gpuMath.step("packToBytes", ["u_position"], "outputBytes");
+        var pixels = new Uint8Array(textureDim*textureDim*4*vectorLength);
+        if (globals.gpuMath.readyToRead()) {
+            globals.gpuMath.readPixels(0, 0, textureDim * vectorLength, textureDim, pixels);
+            var parsedPixels = new Float32Array(pixels.buffer);
+            for (var i=0;i<nodes.length;i++){
+                var rgbaIndex = i*4;
+                var nodePosition = new THREE.Vector3(parsedPixels[rgbaIndex], parsedPixels[rgbaIndex+1], parsedPixels[rgbaIndex+2]);
+                nodes[i].render(nodePosition);
+            }
         }
+
+        //for (var i=0;i<nodes.length;i++){
+        //    var node = nodes[i];
+        //    var rgbaIndex = i*4;
+        //    var nodePosition = new THREE.Vector3(lastPosition[rgbaIndex], lastPosition[rgbaIndex+1], lastPosition[rgbaIndex+2]);
+        //    node.render(nodePosition);
+        //}
         globals.threeView.render();
+        globals.gpuMath.setSize(textureDim, textureDim);
     }
 
     function calcSolveParams(){
@@ -135,7 +163,7 @@ function initDynamicModel(globals){
             if (beam.getNaturalFrequency()>maxFreqNat) maxFreqNat = beam.getNaturalFrequency();
         });
         var _dt = (1/(2*Math.PI*maxFreqNat))*0.5;//half of max delta t for good measure
-        var _numSteps = (1/60)/_dt;
+        var _numSteps = 0.5/_dt;
         return {
             dt: _dt,
             numSteps: _numSteps
@@ -144,7 +172,7 @@ function initDynamicModel(globals){
 
     function initTexturesAndPrograms(gpuMath){
         gpuMath.reset();
-        var textureDim = calcTextureSize(nodes.length);
+        textureDim = calcTextureSize(nodes.length);
 
         var vertexShader = document.getElementById("vertexShader").text;
 
@@ -160,13 +188,14 @@ function initDynamicModel(globals){
         gpuMath.initTextureFromData("u_originalPosition", textureDim, textureDim, "FLOAT", originalPosition);
         gpuMath.initTextureFromData("u_externalForces", textureDim, textureDim, "FLOAT", externalForces);
         gpuMath.initTextureFromData("u_mass", textureDim, textureDim, "FLOAT", mass);
-        gpuMath.initTextureFromData("u_meta", textureDim, textureDim, "float", meta);
+        gpuMath.initTextureFromData("u_meta", textureDim, textureDim, "FLOAT", meta);
         gpuMath.initTextureFromData("u_beamK", textureDim, textureDim, "FLOAT", beamK);
         gpuMath.initTextureFromData("u_beamD", textureDim, textureDim, "FLOAT", beamD);
 
         gpuMath.createProgram("positionCalc", vertexShader, document.getElementById("positionCalcShader").text);
         gpuMath.setUniformForProgram("positionCalc", "u_velocity", 0, "1i");
         gpuMath.setUniformForProgram("positionCalc", "u_lastPosition", 1, "1i");
+        gpuMath.setUniformForProgram("positionCalc", "u_mass", 2, "1i");
         gpuMath.setUniformForProgram("positionCalc", "u_textureDim", [textureDim, textureDim], "2f");
 
         gpuMath.createProgram("velocityCalc", vertexShader, document.getElementById("velocityCalcShader").text);
@@ -184,6 +213,8 @@ function initDynamicModel(globals){
         gpuMath.initTextureFromData("outputBytes", textureDim*4, textureDim, "UNSIGNED_BYTE", null);
         gpuMath.initFrameBufferForTexture("outputBytes");
         gpuMath.setUniformForProgram("packToBytes", "u_floatTextureDim", [textureDim, textureDim], "2f");
+
+        gpuMath.setSize(textureDim, textureDim);
     }
 
     function calcTextureSize(numNodes){
